d5cc55f71198918ac77091853295b971c81f7d88
Taewook Nam <taewook122@gmail.com>
2020-08-20 14:35:44 +0900

    filling button text 변경 기능 추가 :pencil:

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,11 @@
 const canvas = document.querySelector('#jsCanvas'),
     ctx = canvas.getContext('2d'),
-    colors = document.querySelectorAll('.jsColor');
-let isPainting = false; // defines whether it is painting or not
+    colors = document.querySelectorAll('.jsColor'),
+    range = document.querySelector('#jsRange'),
+    mode = document.querySelector('#jsMode');
+
+let isPainting = false, // defines whether it is painting or not
+    isFilling = false; // defines whether current mode is filling or not
 
 canvas.width = 500;
 canvas.height = 500;
@@ -34,6 +38,21 @@ function onColorClick(event) {
     ctx.strokeStyle = color;
 }
 
+function onRangeChange(event) {
+    const strokeSize = event.target.value;
+    ctx.lineWidth = strokeSize;
+}
+
+function onModeClick(event) {
+    if (!isFilling) { // if current mode is filling mode
+        isFilling = true;
+        mode.innerText = "drawing";
+    } else { // if current mode is not drawing mode
+        isFilling = false;
+        mode.innerText = "filling";
+    }
+}
+
 // function onMouseDown(event) {
 //     startPainting();
 // }
@@ -44,20 +63,36 @@ function onColorClick(event) {
 // }
 // deleted because it's useless
 
-if (canvas) { // if canvas exists
-    canvas.addEventListener("mousemove", onMouseMove); // when mouse moves
-    canvas.addEventListener("mousedown", startPainting); // when mouse click begins
-    canvas.addEventListener("mouseup", stopPainting); // when mouse click finishes
-    canvas.addEventListener("mouseleave", stopPainting); // when mouse leaves the canvas
+
+function init() { // init function
+    if (canvas) { // if canvas exists, not null
+        canvas.addEventListener("mousemove", onMouseMove); // when mouse moves
+        canvas.addEventListener("mousedown", startPainting); // when mouse click begins
+        canvas.addEventListener("mouseup", stopPainting); // when mouse click finishes
+        canvas.addEventListener("mouseleave", stopPainting); // when mouse leaves the canvas
+    }
+    
+    if (range) { // if range exist
+        range.addEventListener("input", onRangeChange);
+    }
+    
+    
+    if (colors) {
+        // All the codes below does exactly the same thing!
+    
+        // Arrow Function type 1
+        Array.from(colors).forEach(color => color.addEventListener("click", onColorClick));
+    
+        // Arrow Function type 2
+        // Array.from(colors).forEach((color) => {color.addEventListener("click", onColorClick)});
+    
+        // Immediately-invoked Function type
+        // Array.from(colors).forEach(function(color){color.addEventListener("click", onColorClick)});
+    }
+
+    if (mode) { // if mode exists
+        mode.addEventListener("click", onModeClick);
+    }
 }
 
-// All the codes below does exactly the same thing!
-
-// Arrow Function type 1
-Array.from(colors).forEach(color => color.addEventListener("click", onColorClick));
-
-// Arrow Function type 2
-// Array.from(colors).forEach((color) => {color.addEventListener("click", onColorClick)});
-
-// Immediately-invoked Function type
-// Array.from(colors).forEach(function(color){color.addEventListener("click", onColorClick)});
+init(); // starts code
