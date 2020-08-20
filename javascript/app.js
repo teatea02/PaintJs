2f737d34175820a0413e48a7eeabd468a97d79ae
Taewook Nam <taewook122@gmail.com>
2020-08-20 17:54:34 +0900

    저장 기능 및 채우기 기능 추가 :art:

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,35 @@
+// Initializing Section
+
 const canvas = document.querySelector('#jsCanvas'),
     ctx = canvas.getContext('2d'),
     colors = document.querySelectorAll('.jsColor'),
     range = document.querySelector('#jsRange'),
-    mode = document.querySelector('#jsMode');
+    mode = document.querySelector('#jsMode'),
+    save = document.querySelector('#jsSave');
 
 let isPainting = false, // defines whether it is painting or not
     isFilling = false; // defines whether current mode is filling or not
 
-canvas.width = 500;
-canvas.height = 500;
+const DEFAULT_COLOR = "#34495e",
+    DEFAULT_LINE_WIDTH = 2.5;
 
-ctx.strokeStyle = "#34495e";
-ctx.lineWidth = 2.5;
+const CANVAS_SIZE = [500, 500]; // width, height
+
+// Width and height of canvas
+canvas.width = CANVAS_SIZE[0];
+canvas.height = CANVAS_SIZE[1];
+
+// Initializing color of canvas
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+
+// Initializing color and width of brush
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
+ctx.lineWidth = DEFAULT_LINE_WIDTH;
+
+
+// Function Section
 
 function startPainting() {
     isPainting = true;
@@ -36,6 +54,7 @@ function onMouseMove(event) {
 function onColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function onRangeChange(event) {
@@ -44,13 +63,31 @@ function onRangeChange(event) {
 }
 
 function onModeClick(event) {
-    if (!isFilling) { // if current mode is filling mode
-        isFilling = true;
-        mode.innerText = "drawing";
-    } else { // if current mode is not drawing mode
+    if (isFilling) { // if current mode is filling mode
         isFilling = false;
-        mode.innerText = "filling";
+        mode.innerText = "Draw";
+    } else { // if current mode is not drawing mode
+        isFilling = true;
+        mode.innerText = "Fill";
     }
+}
+
+function onCanvasClick(event) {
+    if (isFilling) { // if current mode is filling mode
+        ctx.fillRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    }
+}
+
+function onRightClick(event) {
+    event.preventDefault(); // disable right click context menu
+}
+
+function onSaveClick(event) {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs";
+    link.click();
 }
 
 // function onMouseDown(event) {
@@ -64,12 +101,14 @@ function onModeClick(event) {
 // deleted because it's useless
 
 
-function init() { // init function
+function main() { // main function
     if (canvas) { // if canvas exists, not null
         canvas.addEventListener("mousemove", onMouseMove); // when mouse moves
         canvas.addEventListener("mousedown", startPainting); // when mouse click begins
         canvas.addEventListener("mouseup", stopPainting); // when mouse click finishes
         canvas.addEventListener("mouseleave", stopPainting); // when mouse leaves the canvas
+        canvas.addEventListener("click", onCanvasClick); // when canvas is clicked
+        canvas.addEventListener("contextmenu", onRightClick); // when canvas is right-clicked
     }
     
     if (range) { // if range exist
@@ -93,6 +132,10 @@ function init() { // init function
     if (mode) { // if mode exists
         mode.addEventListener("click", onModeClick);
     }
+
+    if (save) { // if save exists
+        save.addEventListener("click", onSaveClick);
+    }
 }
 
-init(); // starts code
+main(); // starts code
